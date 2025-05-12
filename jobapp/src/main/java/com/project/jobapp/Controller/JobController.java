e3b4f886d8e7c7c3813933157fe23e4515ba4da0
Pratyush Mathur <109384138+Shoto0095@users.noreply.github.com>
2025-05-13 03:43:54 +0530

    SpringBoot Framework

--- jobapp/src/main/java/com/project/jobapp/Controller/JobController.java
+++ jobapp/src/main/java/com/project/jobapp/Controller/JobController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.jobapp.Service.JobService;
@@ -18,7 +19,7 @@ public class JobController {
     private JobService service;
 
     @GetMapping("posts")
-    // @ResponseBody 
+    @ResponseBody
     public List<JobPost> getAllJobs() {
         return service.getAllJobs();
     }

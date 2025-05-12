b555c6701f155de3a8eb12bf8d6e0ff3e1210dfc
Pratyush Mathur <109384138+Shoto0095@users.noreply.github.com>
2025-05-13 03:38:27 +0530

    React Frontend

--- jobapp/src/main/java/com/project/jobapp/Controller/JobController.java
+++ jobapp/src/main/java/com/project/jobapp/Controller/JobController.java
@@ -3,46 +3,24 @@ package com.project.jobapp.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.jobapp.Service.JobService;
 import com.project.jobapp.model.JobPost;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobController {
 
     @Autowired
     private JobService service;
 
-
-    @GetMapping({"/", "home"})
-    public String home() {
-        return "home";
-    }
-
-
-    @GetMapping("addjob")
-    public String addJob() {
-        return "addjob";
-    }
-
-
-    @PostMapping("handleForm")
-    public String handleForm(JobPost jobPost) {
-        service.addJob(jobPost);
-        return "success";
-
-    }
-
-    @GetMapping("viewalljobs")
-    public String viewJobs(Model m) {
-        List<JobPost> jobs = service.getAllJobs();
-        m.addAttribute("jobPosts", jobs);
-
-        return "viewalljobs";
+    @GetMapping("posts")
+    // @ResponseBody 
+    public List<JobPost> getAllJobs() {
+        return service.getAllJobs();
     }
 
 }
